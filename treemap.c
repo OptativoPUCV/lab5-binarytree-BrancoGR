577a8312e48caabcd196a4eaa4f1539a95342ddc
Your Name <you@example.com>
2024-05-06 15:24:04 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -45,8 +45,8 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 }
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-    TreeNode * new = createTreeNode(key, value);
-    
+    //TreeNode * new = createTreeNode(key, value);
+    return NULL;
 
 }
 
